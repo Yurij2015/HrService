b56d4653f4025442b4166e63f2486cc729ed5bce
Yurij M <Yurij2015@users.noreply.github.com>
2021-03-20 16:59:03 +0300

    Add project files.

---
+++ HrService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HrService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
